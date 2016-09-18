548f0eb5e32064a4dd8123e9c631c83822872397
zhouwentong <zhouwentong1@gmail.com>
2016-09-19 00:30:32 +0800

    Initial commit

---
+++ export_demo.js
@@ -0,0 +1,13 @@
+/**
+ * 导出的 demo
+ * Created by wentong on 2016/9/19.
+ */
+'use strict';
+module.exports = {
+    demo: function (name) {
+        console.log("demo");
+    },
+    demo2: function (name) {
+        console.log("demo2")
+    }
+};
---
+++ node_demo1.js
@@ -0,0 +1,5 @@
+'use strict';
+var r = require('./export_demo');
+var s = 'mmm';
+// 支持通过这种方式调用，这样的话，就可以做成可以配置的了
+r['demo']();

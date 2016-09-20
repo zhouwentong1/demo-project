71d8a2eff1d3852b999af218f7642bd47aec9444
zhouwentong <zhouwentong1@gmail.com>
2016-09-20 23:53:08 +0800

    简单的文件浏览器

---
+++ http_demo2.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var path = require('path');
+
+// process 应该包含标准输入、标准输出的意思、如果没有标准输入，就默认为根目录
+var root = path.resolve(process.argv[2] || '.');
+console.log("static path is : " + root);
+
+var server = http.createServer(function (request, response) {
+    var requestUrl = request.url;
+    var pathname = url.parse(requestUrl).pathname;
+    var s = path.join(root, pathname);
+    fs.stat(s,function (err, data) {
+        // 如果没有错误并且是一个文件的话
+        if (!err && data.isFile()) {
+            console.log('200' + request.url);
+            response.writeHead(200);
+            // 将流重定向到 response 里面去
+            fs.createReadStream(s).pipe(response);
+        } else {
+            console.error(err);
+        }
+    })
+
+});
+server.listen('80');
+
+console.log("server is on~");

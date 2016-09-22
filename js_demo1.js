eb4770496a1513835a7bb463782647b1e997e9a1
zhouwentong <zhouwentong1@gmail.com>
2016-09-22 23:58:09 +0800

    关于 js

---
+++ js_demo1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wentong on 2016/9/22.
+ */
+
+// 比较基本类型的时候最好使用【===】比较，因为【==】会发生类型转换
+function ready() {
+    if (0 === 0) {
+
+    }
+
+    var arr = [];
+    arr.push('aaa');
+    alert(arr)
+}
